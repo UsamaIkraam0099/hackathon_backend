1d560a1f148701573c9a32dc9ca78712c6441104
UsamaIkraam0099 <77922449+UsamaIkraam0099@users.noreply.github.com>
2025-06-26 23:29:36 +0400

    cors and sort addition

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,14 @@ import express from "express";
 
 // others
 import "./config/db";
+import cors from "cors";
 import userRoutes from "./routes/userRoutes";
 import bookRoutes from "./routes/bookRoutes";
 import { errorHandler } from "./middleware/errorMiddleware";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
